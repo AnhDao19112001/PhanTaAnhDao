a6076a58cf6138d64e71d711565afaa3094768fb
Anh Đào <phantaanhdao@gmail.com>
2022-11-09 20:56:58 +0700

    updaet

---
+++ Module1/baitap/ss8_cau_tru_dieu_kien2/dien_tich_hinh_chu-nhat.js
@@ -0,0 +1,6 @@
+let a = +prompt('nhap canh a');
+let b = +prompt('nhap canh b');
+let checkA = a>0 && b>0;
+alert(checkA);
+let dt=a*b;
+alert(dt);
---
+++ Module1/baitap/ss8_cau_tru_dieu_kien2/dien_tich_hinh_vuong.js
@@ -0,0 +1,5 @@
+let a = +prompt('nhap canh a');
+let checkA = a>0;
+alert(checkA);
+let dt=a*a;
+alert(dt);
