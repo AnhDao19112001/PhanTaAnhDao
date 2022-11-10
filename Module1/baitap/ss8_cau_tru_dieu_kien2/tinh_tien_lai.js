2b321ad25984b489b51fcb9220b6044539eeca21
Anh Đào <phantaanhdao@gmail.com>
2022-11-10 21:22:09 +0700

    updaet

--- Module1/baitap/ss8_cau_tru_dieu_kien2/tinh_tien_lai.js
+++ Module1/baitap/ss8_cau_tru_dieu_kien2/tinh_tien_lai.js
@@ -1,9 +1,5 @@
-// (P*R*t*t)/12
-//P là số tiền gốc.
-//R là lãi xuất hàng thang.
-//t là thời gian tiền được đầu tư hoặc vay.
-
-let p = prompt('Nhap so tien goc');
-let R = prompt('Nhap lai xuat hang thang');
-let t = prompt('Nhap thoi gian dau tu hoac vay');
-let laixuat;
+let soTienVay = +prompt("Nhập số tiền bạn muốn vay?");
+let soThangVay = +prompt("Nhập thời gian bạn vay (tháng)");
+let phanTramLaiXuat = +prompt("Nhập lãi xuất(%)");
+let laiXuatHangThang = (soTienVay * phanTramLaiXuat / 100) / soThangVay;
+document.write("Lãi xuất hằng tháng bạn phải trả là: " + laiXuatHangThang + "VND");
