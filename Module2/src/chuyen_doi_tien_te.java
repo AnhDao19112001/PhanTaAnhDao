eec1657a2fe163e330f922c345d23c4f7583aa86
Anh Đào <phantaanhdao@gmail.com>
2022-12-01 21:15:41 +0700

    updaet

---
+++ Module2/src/chuyen_doi_tien_te.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class chuyen_doi_tien_te {
+    public static void main(String[] args) {
+        double vnd = 23000;
+        double usd;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("moi ban nhap so tien usd: ");
+        usd = sc.nextDouble();
+        double quydoi = usd*23000;
+        System.out.println("gia tri vnd: " + quydoi);
+    }
+}
