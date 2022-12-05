e8126be7242e94763ef7257da37f3d79f9bad1ff
Anh Đào <phantaanhdao@gmail.com>
2022-12-05 20:03:24 +0700

    updaet

--- Module2/src/ss3/bai_tap/XoaPhanTuKhoiMang.java
+++ Module2/src/ss3/bai_tap/XoaPhanTuKhoiMang.java
@@ -18,20 +18,14 @@ public class XoaPhanTuKhoiMang {
         }
         System.out.println("Nhập số nguyên k: ");
         int k = scanner.nextInt();
-        // xóa phần tử k ra khỏi mảng
-        // vòng lặp for sẽ khởi tạo c = i = 0
-        // và duyệt i từ 0 đến n
-        // nếu phần tử tại vị trí i khác với số nguyên k
-        // thì gán phần tử tại i cho phần tử tại k
-        // sau đó tăng c lên 1
+        
         for (c = i = 0; i < n; i++) {
             if (A[i] != k) {
                 A[c] = A[i];
                 c++;
             }
         }
-        n = c;  // lúc này số phần tử trong mảng sẽ bằng c
-        // hiển thị các phần tử trong mảng sau khi xóa k
+        n = c;
         System.out.println("Mảng còn lại sau khi xóa phần tử " + k + " là: ");
         for (i = 0; i < n; i++) {
             System.out.print(A[i] + "\t");
