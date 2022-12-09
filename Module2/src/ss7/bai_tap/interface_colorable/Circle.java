b5efd9a81957c0667c5b10f249fcb59aa72d5a1e
Anh Đào <phantaanhdao@gmail.com>
2022-12-09 20:28:06 +0700

    updaet

---
+++ Module2/src/ss7/bai_tap/interface_colorable/Circle.java
@@ -0,0 +1,41 @@
+package ss7.bai_tap.interface_colorable;
+
+public class Circle extends Shape {
+    private double radius = 1.0;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return getRadius() * getRadius() * Math.PI;
+    }
+
+    public double getPerimeter() {
+        return 2 * getRadius() * Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "A Circle with radius="
+                + getRadius()
+                + ", which is a subclass of "
+                + super.toString();
+    }
+}
---
+++ Module2/src/ss7/bai_tap/interface_colorable/colorable.java
@@ -0,0 +1,5 @@
+package ss7.bai_tap.interface_colorable;
+
+public interface colorable {
+    void howToColor();
+}
