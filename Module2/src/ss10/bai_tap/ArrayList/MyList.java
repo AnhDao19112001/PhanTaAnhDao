b45993ada01e909617b6afc1c498b498aa3b5bde
Anh Đào <phantaanhdao@gmail.com>
2022-12-14 15:26:20 +0700

    updaet

--- Module2/src/ss10/bai_tap/ArrayList/MyList.java
+++ Module2/src/ss10/bai_tap/ArrayList/MyList.java
@@ -1,22 +1,90 @@
 package ss10.bai_tap.ArrayList;
-import java.util.Arrays;
-public class MyList {
-    private int size = 0;
-    private static final int DEFAULT_CAPACITY = 10;
-    public Object [] element;
-    public MyList(){
-        element = new Object[DEFAULT_CAPACITY]; //khoi tao mang ban dau voi cd mac dinh
-    }
-    public MyList(int capacity){
-        if (capacity >=10){
-            element = new Object[capacity];
-            }else {
-            throw new
-        }
-        }
-        public void ensureCapacity(){
 
+import java.util.Arrays;
+
+public class MyList<E> {
+    private int size = 0; // so luong phan tu trong mang
+    private static final int DEFAULT_CAPACITY = 10;
+    public Object element[];
+
+    public MyList() {
+      element = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyList(int capacity) {
+        if (capacity >= 0 ){
+            element = new Object[capacity];
+        }else {
+           throw new IllegalArgumentException("capacity" + capacity);
         }
     }
+
+    public int size(){
+        return this.size;
+    }
+
+    public void clear(){
+        size = 0;
+        for (int i = 0; i<element.length; i++){
+            element[i] = null;
+        }
+    }
+
+    public boolean add(E elements){
+        if (element.length == size){
+            this.ensureCapacity(2);
+        }
+        element[size] = elements;
+        size++;
+        return true;
+    }
+
+    public void add(E elements, int index){
+        if (index > element.length){
+            throw new IllegalArgumentException("index:  " + index);
+        }else if (element.length == size){
+            this.ensureCapacity(2);
+        }
+
+       if (element[index] == null){
+           element[index] = elements;
+       }else {
+           for (int i=0;i>= index;i--){
+               element[i] = element[i-1];
+           }
+           element[index] = elements;
+           size++;
+       }
+
+    }
+
+// phuong thuc tang kich thuoc ham
+    public void ensureCapacity(int minCapacity){
+        if (minCapacity >= 0){
+            int newSize = this.element.length + minCapacity;
+            element = Arrays.copyOf(element, newSize);
+        }else {
+            throw new IllegalArgumentException("minCapacity: " + minCapacity );
+        }
+    }
+
+
+
+    public E get(int index){
+        return (E) element[index];
+    }
+
+    public int indexOf(E elements){
+        int index = -1;
+        for (int i =0; i<size;i++){
+            if (this.element[i].equals(elements)){
+                return i;
+            }
+        }
+        return index;
+    }
+
+}
+
 
 
--- Module2/src/ss10/bai_tap/ArrayList/TestMyList.java
+++ Module2/src/ss10/bai_tap/ArrayList/TestMyList.java
@@ -1,4 +1,72 @@
 package ss10.bai_tap.ArrayList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestMyList {
+
+    public static class Student{
+        private int id;
+        private String name;
+
+        public Student() {
+        }
+
+        public Student(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Student{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args) {
+        Student a = new Student(1,"Dao");
+        Student b = new Student(2,"Dat");
+        Student c = new Student(3,"Khai");
+        Student d = new Student(4,"Quan");
+        Student e = new Student(5,"Hoang");
+        Student f = new Student(6,"Truong");
+//            List<String> danhSach =new ArrayList<>();
+//            danhSach.
+
+        MyList<Student> studentMyList = new MyList<>();
+        studentMyList.add(a);
+        studentMyList.add(b);
+        studentMyList.add(c);
+        studentMyList.add(d);
+        studentMyList.add(e);
+        studentMyList.add(f);
+        studentMyList.size();
+
+        for ( int i=0; i<studentMyList.size();i++){
+            Student student = (Student) studentMyList.element[i];
+            System.out.println(student.getId());
+            System.out.println(student.getName());
+
+        }
+        System.out.println(studentMyList.size());
+    }
 }
