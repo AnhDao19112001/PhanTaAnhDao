30d436df403e8ee8686a22d97e361f3b743e436d
Anh Đào <phantaanhdao@gmail.com>
2023-02-18 20:47:36 +0700

    first commit

---
+++ Module2/src/test/giaiThuat1.java
@@ -0,0 +1,4 @@
+package test;
+
+public class giai_thuat_1 {
+}
---
+++ Module2/src/test/giaiThuat2.java
@@ -0,0 +1,8 @@
+package test;
+//Viết chương trình tìm tất cả các số chia hết cho 7 nhưng không phải bội số của 5, nằm trong đoạn 10 và 200 (tính cả 10 và 200).
+// Các số thu được sẽ được in thành chuỗi trên một dòng, cách nhau bằng dấu phẩy.
+public class giaiThuat22 {
+    public static void main(String[] args) {
+        
+    }
+}
