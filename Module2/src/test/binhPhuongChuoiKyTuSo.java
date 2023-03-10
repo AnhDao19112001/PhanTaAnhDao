20fd6667219b509e07f4f8364690a160e47835e4
Anh Đào <phantaanhdao@gmail.com>
2023-03-10 15:12:59 +0700

    first commit

---
+++ Module2/src/test/binhPhuongChuoiKyTuSo.java
@@ -0,0 +1,20 @@
+package test;
+
+import java.util.Scanner;
+
+public class test3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào số cuối dãy ");
+        int lastNumber = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < lastNumber; i++) {
+            if (checkNum(i))
+                System.out.print(i+"\t");
+        }
+    }
+
+    public static boolean checkNum(int number){
+        double sprt = Math.sqrt(number);
+        return sprt == (int)sprt;
+    }
+}
